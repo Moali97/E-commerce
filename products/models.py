683e65f4908ab20fb4f0b5dcd49fce2784ef651b
Syed Ali <syedali3267@gmail.com>
2021-06-28 11:52:40 +0100

    Migrations of new model classess

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ LABEL = (
     ('New', 'New'),
     ('Used', 'Used')
 )
-#
+
 
 # This contains the information about each product
 class Items(models.Model):
@@ -14,18 +14,38 @@ class Items(models.Model):
     label = models.CharField(choices=LABEL, max_length=4, default='')
     description = models.TextField()
 
+    class Meta:
+        verbose_name_plural = "Items"
+
     def __str__(self):
         return self.item_name
-#maybe add get_add and get_remove
 
-class OrderItem(models.Model) :
+
+# maybe add get_add and get_remove
+
+class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Items, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
+    class Meta:
+        verbose_name_plural = "OrderItem"
 
     def __str__(self):
         return f"{self.quantity} of {self.item.item_name}"
 
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Order"
+
+    def __str__(self):
+        return self.user.username
