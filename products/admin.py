857b6c6380b1ff711b117d1938f1122c043afec3
Syed Ali <syedali3267@gmail.com>
2021-06-28 11:53:06 +0100

    Registered new model classess

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Items
+from .models import Items, OrderItem, Order
 
 admin.site.register(Items)
+admin.site.register(OrderItem)
+admin.site.register(Order)
 
 
 
