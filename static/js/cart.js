b885a0fb1c1b077cfc7a1ea0feb51b42f43a3d9e
Syed Ali <syedali3267@gmail.com>
2021-08-06 11:56:21 +0100

    Add to cart functionality in javascript

---
+++ static/js/cart.js
@@ -0,0 +1,11 @@
+    var updateBtns = document.getElementsByClassName('update-cart')
+
+    for(var i = 0; i < updateBtns.length; i++){
+        updateBtns[i].addEventListener('click', function(){
+            var itemID = this.dataset.item
+            var action = this.dataset.action
+            console.log('itemID:', itemID, 'action:', action )
+
+
+        })
+    }
