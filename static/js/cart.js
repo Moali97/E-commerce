6445ba227c1b3e30291830e84d3c79c65efd1925
Syed Ali <syedali3267@gmail.com>
2021-09-02 15:57:55 +0100

    Temporary code

--- static/js/cart.js
+++ static/js/cart.js
@@ -2,15 +2,37 @@
 
     for(var i = 0; i < updateBtns.length; i++){
         updateBtns[i].addEventListener('click', function(){
-            var itemID = this.dataset.item
+            var itemId = this.dataset.item
             var action = this.dataset.action
-            console.log('itemID:', itemID, 'action:', action )
+            console.log('itemId:', itemId, 'action:', action )
 
             console.log('USER:', user)
             if(user === 'AnonymousUser'){
                 console.log('you are not logged in')
             }else{
-                console.log('user is logged in, sending requested data')
+                updateUserOrder(itemId, action)
             }
         })
+    }
+
+    function updateUserOrder(itemId, action){
+        console.log('user is logged in, sending requested data')
+
+        var url = 'update_item/'
+
+        fetch(url, {
+            method:'POST',
+            headers:{
+                 'Content-Type':'application/json',
+                 'X-CSRFToken': csrftoken,
+            },
+            body:JSON.stringify({'itemId': itemId, 'action':action})
+        })
+        .then((response) =>{
+            return response.json
+        })
+        .then((data) =>{
+            console.log('data:',data)
+        })
+
     }
